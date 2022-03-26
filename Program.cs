6298412e20d090173422e3e502d5f995f63b59a7
aswinthottilpalam <aswinp264@gmail.com>
2022-03-26 23:21:28 +0530

    User registration using lambda function

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace UserRegistrationUsingLambda
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to User Registration Using Lambda Expression");
+        }
+    }
+}
